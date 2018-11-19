2dbbf9d8af5475b8bbe0561947f3e25a7172c607
Thib <thibaut.g@protonmail.com>
2018-11-19 16:53:39 +0100

    Use env var for USER_NAME

--- test_site/views.py
+++ test_site/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.views import generic
 
@@ -8,4 +9,5 @@ class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['msg'] = 'Hello world!'
+        context['name'] = os.environ.get('USER_NAME', 'Unknown')
         return context
