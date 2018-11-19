da69962e7d8373378a83cf1394b03ed7f9a2c7cd
Thib <thibaut.g@protonmail.com>
2018-11-19 23:37:33 +0100

    Better startup shelll

--- build-run.sh
+++ build-run.sh
@@ -1,3 +1,8 @@
-s2i build . registry.access.redhat.com/rhscl/python-36-rhel7 django-s2i-image
-docker run --rm -ti --name django-s2i-container -p 8080:8080 django-s2i-image
+export image_name=django-s2i-image
+# export base_image=centos/python-36-centos7
+export base_image=registry.access.redhat.com/rhscl/python-36-rhel7
+s2i build . $base_image $image_name
+docker run --rm -ti --name django-s2i-container -p 8080:8080 $image_name
+docker rmi $image_name
+# docker rmi $base_image
 docker image prune --force
