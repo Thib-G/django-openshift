5370b4e0f48951334d85687dde1eeea4ffe4067e
Thib <thibaut.g@protonmail.com>
2018-11-19 22:29:50 +0100

    Renamre build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,2 @@
 s2i build . registry.access.redhat.com/rhscl/python-36-rhel7 django-s2i-image
-docker run --rm --name django-s2i-container -p 8080:8080 django-s2i-image
+docker run --rm -ti --name django-s2i-container -p 8080:8080 django-s2i-image
